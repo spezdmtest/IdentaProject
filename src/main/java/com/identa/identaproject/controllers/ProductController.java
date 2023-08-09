0c1b489986514bb2feb9c25fbebbb739dccd6fee
spezdmtest <spezd.m.@gmail.com>
2023-08-09 16:32:51 +0300

    Create Product @Controller @Service

--- src/main/java/com/identa/identaproject/controllers/ProductController.java
+++ src/main/java/com/identa/identaproject/controllers/ProductController.java
@@ -1,10 +1,11 @@
 package com.identa.identaproject.controllers;
 
 import com.identa.identaproject.dto.ProductDTO;
-import com.identa.identaproject.service.ProductServiceImpl;
+import com.identa.identaproject.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,10 +15,11 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductServiceImpl productServiceImpl;
+    private final ProductService productService;
 
+    @GetMapping
     public String list(Model model){
-        List<ProductDTO> listProducts = productServiceImpl.getAll();
+        List<ProductDTO> listProducts = productService.getAll();
         model.addAttribute("products", listProducts);
         return "products";
     }
